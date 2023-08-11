cff7ac5116ba618846fca91e2fe6ca6fc03e8875
Anton Spelec <dezomaradzelezo@seznam.cz>
2023-08-11 18:55:19 +0200

    Build 1

--- GUI/Components/ContextMenu.cs
+++ GUI/Components/ContextMenu.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using ImGuiNET;
+using libLSD.Formats; // Pøidáno pro práci s LBD a TMD
 
 namespace LSDView.GUI.Components
 {
@@ -19,6 +20,9 @@ namespace LSDView.GUI.Components
                     if (ImGui.Selectable(item.Key)) item.Value();
                 }
 
+                // Pøidání položky pro nahrazení modelu
+                if (ImGui.Selectable("Replace Model")) ReplaceModelMenuItem_Click();
+
                 ImGui.EndPopup();
             }
         }
@@ -28,5 +32,14 @@ namespace LSDView.GUI.Components
         public override bool Equals(object obj) { return obj is ContextMenu other && Equals(other); }
 
         public override int GetHashCode() { return (MenuItems != null ? MenuItems.GetHashCode() : 0); }
+
+        // Metoda pro spuštìní funkce nahrazení modelu
+        private void ReplaceModelMenuItem_Click()
+        {
+            // TODO: Zde bude kód pro nahrazení modelu.
+            // Mùžete napøíklad vytvoøit instanci tøídy LBDModelReplacer a zavolat její metodu.
+            // LBDModelReplacer replacer = new LBDModelReplacer();
+            // replacer.ReplaceModel(...);
+        }
     }
 }
---
+++ GUI/LBDModelReplacer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using libLSD.Formats;
+
+public class LBDModelReplacer
+{
+    private LBD lbdFile;
+    private TMD newTMDModel;
+
+    public void ReplaceModelFromTMD(string lbdFilePath, string tmdFilePath, int modelIndex)
+    {
+        int lbdSize;
+        int tmdSize;
+
+        // Load the LBD file
+        using (FileStream lbdFileStream = new FileStream(lbdFilePath, FileMode.Open))
+        using (BinaryReader lbdReader = new BinaryReader(lbdFileStream))
+        {
+            lbdFile = new LBD(lbdReader);
+
+            // Read LBD size from bytes 0A-0D
+            lbdFileStream.Seek(0x0A, SeekOrigin.Begin);
+            lbdSize = lbdReader.ReadInt32();
+        }
+
+        // Load the TMD file
+        using (FileStream tmdFileStream = new FileStream(tmdFilePath, FileMode.Open))
+        using (BinaryReader tmdReader = new BinaryReader(tmdFileStream))
+        {
+            newTMDModel = new TMD(tmdReader);
+
+            // Read TMD size from its header (based on the TMD format specification)
+            tmdFileStream.Seek(0x04, SeekOrigin.Begin);
+            tmdSize = tmdReader.ReadInt32();
+        }
+
+        // Check size constraints
+        if (tmdSize > lbdSize)
+        {
+            MessageBox.Show("The new model is larger than the original. Replacement not allowed.");
+            return;
+        }
+
+        // Replace the model at the specified index
+        // NOTE: You'll need to implement a way to replace the model in the LBD file.
+        // The following is just a placeholder and will not compile.
+        // lbdFile.Models[modelIndex] = newTMDModel;
+
+        // Save the modified LBD file
+        using (FileStream saveFileStream = new FileStream(lbdFilePath, FileMode.Create))
+        using (BinaryWriter saveWriter = new BinaryWriter(saveFileStream))
+        {
+            lbdFile.Write(saveWriter);
+        }
+    }
+}
